fd58df38a6c925e35ee0e6c893aa3cf1dab5073a
grey <charminggw@gmail.com>
2022-07-17 21:40:00 +0900

    init commit

---
+++ book/goodcode-badcode/src/test/java/com/example/goodcodebadcode/GoodcodeBadcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodcodebadcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodcodeBadcodeApplicationTests {
+
+        @Test
+        void contextLoads () {
+        }
+
+}
